11bda6a27c1d683a58db324223f751d1b3e3ddd3
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-09-09 19:31:02 +0200

    Changed Configuration CORS POLICY

--- src/main/java/it/smartcitywastemanagement/citizenms/configuration/WebAppConfiguration.java
+++ src/main/java/it/smartcitywastemanagement/citizenms/configuration/WebAppConfiguration.java
@@ -1,8 +1,10 @@
 package it.smartcitywastemanagement.citizenms.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebAppConfiguration implements WebMvcConfigurer {
 
 
